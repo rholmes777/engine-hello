f05085a448ef5a00b13b4487f45c9c504b562470
Richard Holmes <rholmes@richardholmes.net>
2012-04-14 20:22:26 -0500

    Add Jinja2 templates.

--- helloworld.py
+++ helloworld.py
@@ -4,6 +4,11 @@ import datetime
 import urllib
 import webapp2
 import logging
+import jinja2
+import os
+
+jinja_environment = jinja2.Environment(
+  loader = jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 # Google imports
 from google.appengine.ext import db
@@ -18,6 +23,30 @@ from google.appengine.api import users
 # TODO the tutorial states: the rate at which you can write to the same entity group
 # is limited to 1 write to the entity group per second.  WHY?
 
+# guestbook_name is a parameter in the URL; the parent of a Greeting is
+# a guestbook: 'Guestbook: name=Foo barbaz'
+# The parent is used as a placeholder for transaction and consistency purposes. 
+# Objects that share a common ancestor belong to the same entity group. 
+
+# Ancestor Queries, as shown here, are strongly consistent with the High
+# Replication datastore. Queries that span entity groups are eventually
+# consistent. If we omitted the ancestor from this query there would be a
+# slight chance that Greeting that had just been written would not show up
+# in a query.
+#
+# The only supported queries are those whose performance scales with the size
+# of the result set (as opposed to the data set). (Better scalability)
+
+    # Sample query for something which uses the Author filter    
+#     another_greeting = db.GqlQuery("SELECT * "
+#                             "FROM Greeting "
+#                             "WHERE ANCESTOR IS :1 "
+#                             "AND Author = :2 "
+#                             "ORDER BY date DESC LIMIT 10 ",
+#                             'joe',
+#                             'test@example.com')
+#     logging.debug("Another greeting using vars(): %s" % vars(another_greeting))                            
+
 
 class Greeting(db.Model):
   """Models an individual Guestbook entry with an author, content, and date."""
@@ -31,60 +60,27 @@ def guestbook_key(guestbook_name=None):
 
 class MainPage(webapp2.RequestHandler):
   def get(self):
-    self.response.out.write('<html><body>')
-    
-    # guestbook_name is a parameter in the URL; the parent of a Greeting is
-    # a guestbook: 'Guestbook: name=Foo barbaz'
-    # The parent is used as a placeholder for transaction and consistency purposes. 
-    # Objects that share a common ancestor belong to the same entity group. 
-    guestbook_name = self.request.get('guestbook_name')
-    logging.info("********* Guestbook name: %s\n" % guestbook_name)
-    
-    # Ancestor Queries, as shown here, are strongly consistent with the High
-    # Replication datastore. Queries that span entity groups are eventually
-    # consistent. If we omitted the ancestor from this query there would be a
-    # slight chance that Greeting that had just been written would not show up
-    # in a query.
-    #
-    # The only supported queries are those whose performance scales with the size
-    # of the result set (as opposed to the data set). (Better scalability)
-    greetings = db.GqlQuery("SELECT * "
-                            "FROM Greeting "
-                            "WHERE ANCESTOR IS :1 "
-                            "ORDER BY date DESC LIMIT 10",
-                            guestbook_key(guestbook_name))
-    
-    for greeting in greetings:
-      # Author is email address; in test env, it's often 'test@example.com'
-      if greeting.author:
-        self.response.out.write('<b>%s</b> wrote: ' % greeting.author.nickname())
-      else: 
-        self.response.out.write('An anonymous person wrote:')
-      self.response.out.write('<blockquote>%s</blockquote>' % cgi.escape(greeting.content))
-    
-    # Sample query for something which uses the Author filter    
-    another_greeting = db.GqlQuery("SELECT * "
-                            "FROM Greeting "
-                            "WHERE ANCESTOR IS :1 "
-                            "AND Author = :2 "
-                            "ORDER BY date DESC LIMIT 10 ",
-                            'joe',
-                            'test@example.com')
-    logging.debug("Another greeting using vars(): %s" % vars(another_greeting))                            
+    guestbook_name=self.request.get('guestbook_name')
+    greetings_query = Greeting.all().ancestor(
+      guestbook_key(guestbook_name)).order('-date')
+    greetings = greetings_query.fetch(10)
+  
+    if users.get_current_user():
+      url = users.create_logout_url(self.request.uri)
+      url_linktext = 'Logout'
+    else:
+      url = users.create_login_url(self.request.uri)
+      url_linktext = 'Login'
+  
+    template_values = {
+      'greetings': greetings,
+      'url': url,
+      'url_linktext': url_linktext,
+    }
 
-    self.response.out.write("""
-          <form action="/sign?%s" method="post">
-            <div><textarea name="content" rows="3" cols="60"></textarea></div>
-            <div><input type="submit" value="Sign Guestbook"></div>
-          </form>
-          <hr>
-          <form>Guestbook name: <input value="%s" name="guestbook_name">
-          <input type="submit" value="switch"></form>
-        </body>
-      </html>""" % (urllib.urlencode({'guestbook_name': guestbook_name}),
-                          cgi.escape(guestbook_name)))
-    
-
+    template = jinja_environment.get_template('index.html')
+    self.response.out.write(template.render(template_values))
+  
 class Guestbook(webapp2.RequestHandler):
   def post(self):
     # We set the same parent key on the 'Greeting' to ensure each greeting is in
